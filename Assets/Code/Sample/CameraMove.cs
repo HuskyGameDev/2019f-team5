9bcbb857a5765a9f61f13790d0a0f2c41ec2f208
Parker Ackerman <PAckerman>
2019-10-23 18:30:54 -0400

    Please fix Unity gods

--- Assets/Code/Sample/CameraMove.cs
+++ Assets/Code/Sample/CameraMove.cs
@@ -14,10 +14,10 @@ public class CameraMove : MonoBehaviour
 		float x = 0.0f;
 		float y = 0.0f;
 
-		if (InputManager.GetKey(KeyCode.A)) x -= 1.0f;
-		if (InputManager.GetKey(KeyCode.D)) x += 1.0f;
-		if (InputManager.GetKey(KeyCode.W)) y += 1.0f;
-		if (InputManager.GetKey(KeyCode.S)) y -= 1.0f;
+		if (InputManager.GetAxis("CamLeft") > Mathf.Epsilon) x -= 1.0f;
+		if (InputManager.GetAxis("CamRight") > Mathf.Epsilon) x += 1.0f;
+		if (InputManager.GetAxis("CamUp") > Mathf.Epsilon) y += 1.0f;
+		if (InputManager.GetAxis("CamDown") > Mathf.Epsilon) y -= 1.0f;
 
 		Vector2 move = new Vector2(x, y);
 		move *= (speed * Time.deltaTime);
