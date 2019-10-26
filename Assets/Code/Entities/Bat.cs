3c05ff94f1bf0521c48b1ce3251e06a1442d1daf
Jacob Zetty <jzetty@lab.huskygames.com>
2019-10-26 19:38:08 -0400

    Added a bat AI

--- Assets/Code/Entities/Bat.cs
+++ Assets/Code/Entities/Bat.cs
@@ -3,23 +3,54 @@
 //
 
 using UnityEngine;
+using System;
 
 public class Bat : Entity
 {
 
 	public float jumpVelocity;
-	public float gravity = 0;
-
-	private void Update()
+	public float gravity;
+	public bool aggro;
+	[SerializeField]
+	public GameObject player;
+	
+	private void Start()
 	{
-		Vector2 accel = new Vector2(Input.GetAxisRaw("Horiz"), 0.0f);
+		player = GameObject.Find("Player");
+	}
+    private void Update()
+	{
+		float PlayerY = player.transform.position.y;
+		float PlayerX = player.transform.position.x;
 
-		if ((colFlags & CollisionFlags.Below) != 0)
+		if(Math.Abs(PlayerX - transform.position.x) <= 6 && Math.Abs(PlayerY - transform.position.y) < 6)
 		{
-			if (Input.GetKey(KeyCode.Space))
-				velocity.y = jumpVelocity;
+			aggro = true;
 		}
 
-	//	Move(world, accel, gravity);
+		if(Math.Abs(PlayerX - transform.position.x) >= 10 && Math.Abs(PlayerY - transform.position.y) >= 10)
+		{
+			aggro = false;
+		}
+
+		Vector2 accel = Vector2.zero;
+
+		if(PlayerX < transform.position.x && aggro)
+		{
+			accel += Vector2.left;
+		} else if (aggro)
+		{
+			accel += Vector2.right;
+		}
+		
+		if(PlayerY < transform.position.y && aggro)
+		{
+			accel += Vector2.down;
+		} else if (aggro)
+		{
+			accel += Vector2.up;
+		}
+
+		Move(world, accel, 0);
 	}
 }
