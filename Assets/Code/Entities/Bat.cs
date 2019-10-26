96b0644146d0f8cee83ad13ea674c18c0cf63836
Jacob Zetty <jzetty@lab.huskygames.com>
2019-10-26 18:10:16 -0400

    Added two bats, two rats, and a slime

---
+++ Assets/Code/Entities/Bat.cs
@@ -0,0 +1,26 @@
+﻿//
+// When We Fell
+//
+
+using UnityEngine;
+
+public class Bat : Entity
+{
+	public World world;
+
+	public float jumpVelocity;
+	public float gravity = 0;
+
+	private void Update()
+	{
+		Vector2 accel = new Vector2(Input.GetAxisRaw("Horiz"), 0.0f);
+
+		if ((colFlags & CollisionFlags.Below) != 0)
+		{
+			if (Input.GetKey(KeyCode.Space))
+				velocity.y = jumpVelocity;
+		}
+
+	//	Move(world, accel, gravity);
+	}
+}
---
+++ Assets/Code/Entities/Rat.cs
@@ -0,0 +1,26 @@
+﻿//
+// When We Fell
+//
+
+using UnityEngine;
+
+public class Rat : Entity
+{
+	public World world;
+
+	public float jumpVelocity;
+	public float gravity;
+
+    private void Update()
+	{
+		Vector2 accel = new Vector2(Input.GetAxisRaw("Horiz"), 0.0f);
+
+		if ((colFlags & CollisionFlags.Below) != 0)
+		{
+			if (Input.GetKey(KeyCode.Space))
+				velocity.y = jumpVelocity;
+		}
+
+		Move(world, accel, gravity);
+	}
+}
