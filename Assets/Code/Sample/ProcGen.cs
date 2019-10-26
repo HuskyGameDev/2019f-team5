1bc7a021b605c8ff7704a0a1bb71a2b363475a84
theNewton17 <Newton17.LuxCraft@gmail.com>
2019-10-26 17:17:44 -0400

    finished basic solution path generation

--- Assets/Code/Sample/ProcGen.cs
+++ Assets/Code/Sample/ProcGen.cs
@@ -10,55 +10,93 @@ public class ProcGen
         //first two dimensions are coords of metatiles/rooms
         //second two dimensions are coords within each metatile/room
         int[,,,] level = new int[4,4,16,16];
-
+        makeSolutionPath(level);
         
     }
 
-    private void solutionPath(int[,,,] level)
+    private void makeSolutionPath(int[,,,] level)
     {
         int roomX = Random.Range(0, 4);
         int roomY = 0;
 
         int direction = 0;
         int prevRoom = 0;
+        level[roomX, roomY, 0, 0] = 1;
 
-        while (roomY < 4)
+        while (roomY < level.GetLength(1))
         {
             direction = Random.Range(0,5);
-            level[roomX, roomY, 0, 0] = 1;
 
             if (direction == 0 || direction == 1)
             {
                 //go left
-                goLeft(level, ref roomX, ref roomY);
+                goLeft(level, ref roomX, ref roomY, ref prevRoom);
 
             } else if (direction == 2 || direction == 3)
             {
                 //go right
-                goRight(level, ref roomX, ref roomY);
+                goRight(level, ref roomX, ref roomY, ref prevRoom);
 
             } else
             {
                 //go down
-                goDown(level, ref roomX, ref roomY);
+                goDown(level, ref roomX, ref roomY, ref prevRoom);
             }
+
+            
         }
 
         
     }
 
-    private void goLeft(int[,,,] level, ref int roomX, ref int roomY)
+    private void goLeft(int[,,,] level, ref int roomX, ref int roomY, ref int prev)
     {
+        if (roomX == 0) {
+            goDown(level, ref roomX, ref roomY, ref prev);
+            return;
+        }
 
+        if (prev == 2) {
+            level[roomX, roomY, 0, 0] = 3;
+            prev = 3;
+        } else {
+            level[roomX, roomY, 0, 0] = 1;
+            prev = 1;
+        }
+        roomX--;
     }
 
-    private void goRight(int[,,,] level, ref int roomX, ref int roomY)
+    private void goRight(int[,,,] level, ref int roomX, ref int roomY, ref int prev)
     {
+        if (roomX == level.GetLength(0) - 1) {
+            goDown(level, ref roomX, ref roomY, ref prev);
+            return;
+        }
 
+        if (prev == 2) {
+            level[roomX, roomY, 0, 0] = 3;
+            prev = 3;
+        } else {
+            level[roomX, roomY, 0, 0] = 1;
+            prev = 1;
+        }
+        roomX++;
     }
 
-    private void goDown(int[,,,] level, ref int roomX, ref int roomY)
+    private void goDown(int[,,,] level, ref int roomX, ref int roomY, ref int prev)
     {
+        if (roomY == level.GetLength(1) - 1) {
+            roomY++;
+            return;
+        }
 
+        if (prev == 2) {
+            level[roomX, roomY, 0, 0] = 4;
+            prev = 4;
+        } else {
+            level[roomX, roomY, 0, 0] = 2;
+            prev = 2;
+        }
+        roomY++;
     }
 }
