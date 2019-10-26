9048c8c33f4465aebae6aa62b92e7a1394126a65
Matt Kuhn <mkuhn@lab.huskygames.com>
2019-10-26 17:52:17 -0400

    added skeleton and arrow

---
+++ Assets/Code/Entities/Arrow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : Entity
+{
+  
+    public World world;
+    public float gravity;
+    Player target;
+    Vector2 moveDirection;
+    void Start(){
+        target = GameObject.FindObjectOfType<Player>();
+        moveDirection = (target.transform.position - transform.position).normalized;
+        Move(world, moveDirection, gravity);
+        Destroy (gameObject, 3f);
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo){
+        if(hitInfo.gameObject.name.Equals("Player") ){
+            Destroy(gameObject);
+        }
+        
+    }
+    
+}
