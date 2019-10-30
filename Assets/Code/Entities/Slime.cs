fbbe19c4c770dde0df27fd39ef4506005ddfabbb
Jacob Zetty <jzetty@lab.huskygames.com>
2019-10-30 19:10:18 -0400

    I added the slime's AI

--- Assets/Code/Entities/Slime.cs
+++ Assets/Code/Entities/Slime.cs
@@ -3,23 +3,56 @@
 //
 
 using UnityEngine;
+using System;
 
 public class Slime : Entity
 {
-
 	public float jumpVelocity;
-	public float gravity = 0;
-
-	private void Update()
+	public float gravity;
+	public bool aggro;
+	public float jumpTime = 1.5f;
+	[SerializeField]
+	public GameObject player;
+	
+	private void Start()
 	{
-		Vector2 accel = new Vector2(Input.GetAxisRaw("Horiz"), 0.0f);
+		player = GameObject.Find("Player");
+	}
+    private void Update()
+	{	
+		jumpTime -= Time.deltaTime;
 
-		if ((colFlags & CollisionFlags.Below) != 0)
+		float PlayerY = player.transform.position.y;
+		float PlayerX = player.transform.position.x;
+
+		if(Math.Abs(PlayerX - transform.position.x) <= 8 && Math.Abs(PlayerY - transform.position.y) < 8)
 		{
-			if (Input.GetKey(KeyCode.Space))
-				velocity.y = jumpVelocity;
+			aggro = true;
 		}
 
-	//	Move(world, accel, gravity);
+		if(Math.Abs(PlayerX - transform.position.x) >= 15 && Math.Abs(PlayerY - transform.position.y) >= 15)
+		{
+			aggro = false;
+		}
+
+		Vector2 accel = Vector2.zero;
+
+		if(PlayerX < transform.position.x && aggro && (colFlags & CollisionFlags.Below) == 0)
+		{
+			accel += Vector2.left;
+		} else if (PlayerX > transform.position.x && aggro && (colFlags & CollisionFlags.Below) == 0)
+		{	
+			accel += Vector2.right;
+		}
+		
+		if(aggro && (colFlags & CollisionFlags.Below) != 0)
+		{
+			if(jumpTime <= 0) {
+				velocity.y = jumpVelocity;
+				jumpTime = 1.5f;
+			}
+		}
+
+		Move(world, accel, gravity);
 	}
 }
