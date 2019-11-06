74e0d726342a1974c7c1e76fae2f376187ba6b2c
Jacob Zetty <jzetty@lab.huskygames.com>
2019-11-06 18:48:57 -0500

    Added an aggro range to the skeleton and allowed him to move. Also changed the skeleton and arrow sprite

--- Assets/Code/Entities/Skeleton.cs
+++ Assets/Code/Entities/Skeleton.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Skeleton : Entity
 {
     public GameObject Arrow;
     float fireRate;
     float nextFire;
+    public float jumpVelocity;
+	public float gravity;
+	public bool aggro;
+	[SerializeField]
+	public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +26,43 @@ public class Skeleton : Entity
     void Update()
     {
         FireOrNot();
+
+        float PlayerY = player.transform.position.y;
+		float PlayerX = player.transform.position.x;
+
+		if(Math.Abs(PlayerX - transform.position.x) <= 14 && Math.Abs(PlayerY - transform.position.y) < 8)
+		{
+			aggro = true;
+		}
+
+		if(Math.Abs(PlayerX - transform.position.x) >= 16 || Math.Abs(PlayerY - transform.position.y) >= 16)
+		{
+			aggro = false;
+		}
+
+		Vector2 accel = Vector2.zero;
+
+		if(PlayerX < transform.position.x && aggro)
+		{
+			accel += Vector2.left;
+		} else if (aggro)
+		{
+			accel += Vector2.right;
+		}
+		
+		if(PlayerY < transform.position.y && aggro)
+		{
+			accel += Vector2.down;
+		} else if (aggro)
+		{
+			accel += Vector2.up;
+		}
+
+		Move(world, accel, -30);
     }
 
     void FireOrNot(){
-        if(Time.time > nextFire){
+        if(Time.time > nextFire && aggro){
             Instantiate(Arrow, transform.position, Quaternion.identity);
             nextFire = Time.time + fireRate;
         }
