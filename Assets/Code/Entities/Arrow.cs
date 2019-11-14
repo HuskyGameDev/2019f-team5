e67efbd34492e4d1c3bf58c4893bdb7240bf79ae
Jacob Zetty <jzetty@lab.huskygames.com>
2019-11-13 19:58:19 -0500

    made it so that the arrow will aim towards the player rather than staying horizontal

--- Assets/Code/Entities/Arrow.cs
+++ Assets/Code/Entities/Arrow.cs
@@ -14,10 +14,20 @@ public class Arrow : Entity
 			target = GameObject.FindWithTag("Player").GetComponent<Player>();
 
         moveDirection = (target.transform.position - transform.position).normalized * speed;
+        float angle;
+        if(target.transform.position.x < transform.position.x)
+        {
+            angle = 180 + Mathf.Atan2 (target.transform.position.y - transform.position.y, target.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
+        } else
+        {
+            angle = Mathf.Atan2 (target.transform.position.y - transform.position.y, target.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
+        }
+        this.transform.rotation = Quaternion.Euler (new Vector3(0, 0, angle));
     }
 
     void Update()
 	{
+        
         Move(world, moveDirection, gravity);
     }
 
