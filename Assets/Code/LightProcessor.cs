c85104ffbbab37e545a18dd3e79d2c728519c281
JasonBricco <jasonbricco@gmail.com>
2020-01-24 17:15:39 -0500

    Began work on a custom lighting system.

---
+++ Assets/Code/LightProcessor.cs
@@ -0,0 +1,25 @@
+﻿//
+// When We Fell
+//
+
+using UnityEngine;
+
+public sealed class LightProcessor : MonoBehaviour
+{
+	public Material mat;
+	public Camera scanner;
+
+	private void Start()
+		=> CreateLightmap();
+
+	private void CreateLightmap()
+	{
+		RenderTexture lightmap = new RenderTexture(Screen.width, Screen.height, 0);
+		scanner.targetTexture = lightmap;
+
+		mat.SetTexture("_Lightmap", lightmap);
+	}
+
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+		=> Graphics.Blit(source, destination, mat);
+}
