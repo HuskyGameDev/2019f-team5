b3e05bb37511d833dbdc5cfdfa3012c71600df9a
Jacob Zetty <jzetty@lab.huskygames.com>
2020-01-24 16:50:54 -0500

    added a small fix to the rat AI

--- Assets/Code/Entities/Rat.cs
+++ Assets/Code/Entities/Rat.cs
@@ -31,7 +31,7 @@ public class Rat : Entity
 		float PlayerY = player.transform.position.y;
 		float PlayerX = player.transform.position.x;
 
-		if(Math.Abs(PlayerX - transform.position.x) <= 10 && Math.Abs(PlayerY - transform.position.y) < 1.2)
+		if(Math.Abs(PlayerX - transform.position.x) <= 10 && Math.Abs(PlayerY - transform.position.y) < 3.2)
 		{
 			aggro = true;
 		}
