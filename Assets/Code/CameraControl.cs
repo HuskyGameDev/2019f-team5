aa616acc8e94dfb87449e8c86708b159ac4f71ad
jwkurtz <jwkurtz@mtu.edu>
2020-02-07 18:11:25 -0500

    Fixes camera clamping issues

--- Assets/Code/CameraControl.cs
+++ Assets/Code/CameraControl.cs
@@ -6,78 +6,58 @@ using System;
 // This prevents the player from seeing "off the map" and increases the atmospheric feel of the game.
 public class CameraControl : MonoBehaviour{
 
-    Transform player;               // OUR TARGET TO TRACK
-    float xOffset = 0;              // camera's X offset from the player
-    float yOffset = 0;              // camera's Y offset from the player
-    float followSpeed = 100000;      // speed that the camera will follow the player
-    bool xLocked = false;           // whether or not the camera is locked on the current X axis
-    bool yLocked = false;           // whether or not the camera is locked on the current Y axis
-    bool playerLocked = false;
-    World world;
-    RectInt levelBounds;
-
-
-    // Use this for initialization
+    //private Camera mainCamera;
+    private Transform player;           // OUR TARGET TO TRACK
+    private float xOffset = 0;          // camera's X offset from the player
+    private float yOffset = 0;          // camera's Y offset from the player
+    private float followSpeed;          // speed that the camera will follow the player
+    private World world;                // world object for retrieving bounds
+    private RectInt levelBounds;        // outer bounds of the generated level
+    private float cameraOrthoSize;      // distance from center of camera to outer Y bounds
+    private float cameraHeight;         // essentially orthoSize
+    private float cameraWidth;          // height * adjustment aspect ratio
+    
     void Start(){
-               
-        // subscribes the "playerSpawned" event to our method
-        // that will set the focus of our camera on the player
-        // EventManager.Instance.Subscribe(GameEvent.PlayerSpawned, onPlayerSpawned);
-
+        
         // locks onto and snaps camera to the player on initialization
         player = GameObject.FindWithTag("Player").transform;
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
 
-        // gets the world, and subscribes to the world-spawned event
+        // gets the world, and subscribes to the world-spawned and player-spawned event
         world = GameObject.FindWithTag("Manager").GetComponent<World>();
-        EventManager.Instance.Subscribe(GameEvent.LevelGenerated, OnWorldLoaded);
-        followSpeed = 5;
+        EventManager.Instance.Subscribe( GameEvent.LevelGenerated, OnWorldLoaded );
+        EventManager.Instance.Subscribe( GameEvent.PlayerSpawned, OnPlayerSpawned );
     }
 
     // Update is called once per frame
     void Update(){
-
-        if ( ! playerLocked ){
-            transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
-            return;
-        }
-
+        
+        // gets the player's new location, adjusts for follow speed
         float playerX = player.position.x + xOffset;
         float playerY = player.position.y + yOffset;
-        float newX = transform.position.x;
-        float newY = transform.position.y;
+        float newX = Mathf.Lerp( transform.position.x, playerX, Time.deltaTime * followSpeed );
+        float newY = Mathf.Lerp( transform.position.y, playerY, Time.deltaTime * followSpeed );
 
-        // locks camera for reaching the edge of the left and right of the dungeon
-        if ( !xLocked && (transform.position.x + 5 > levelBounds.xMax ||
-                          transform.position.x - 5 < levelBounds.xMin)) {
-            xLocked = true;
-        }
-
-        // locks camera for reaching the edge of the top and bottom of the dungeon
-        if (yLocked && (transform.position.y + 5 <= levelBounds.yMax ||
-                        transform.position.y - 5 <= levelBounds.yMin)) {
-            yLocked = false;
-        }
-        
-        // updates 
-        if (!xLocked) {
-            newX = Mathf.Lerp(transform.position.x, playerX, Time.deltaTime * followSpeed);
-        }
-        if (!yLocked) {
-            newY = Mathf.Lerp(transform.position.y, playerY, Time.deltaTime * followSpeed);
-        }
+        // clamps camera to edge of screen when at the outer bounds of the map
+        newX = Mathf.Clamp( newX, levelBounds.xMin + cameraWidth, levelBounds.xMax - cameraWidth);
+        newY = Mathf.Clamp( newY, levelBounds.yMin + cameraHeight , levelBounds.yMax - cameraHeight);
 
         // updates camera position
         transform.position = new Vector3(newX, newY, transform.position.z);
     }
-
-    // sets the player to the target to track
-    //public void onPlayerSpawned(object playerObject){
-    //    targetToTrack = GameObject.FindWithTag("Player").transform;
-    //}
-
+    
     // retrieves the world's level bounds once the procedural generation is complete
-    public void OnWorldLoaded(object worldobject) {
+    private void OnWorldLoaded(object worldObject) {
         levelBounds = world.GetBounds();
+
+        // camera clamp setup
+        cameraOrthoSize = 12;                           // hard-coded in for now
+        cameraHeight = cameraOrthoSize;
+        cameraWidth = (cameraOrthoSize) * (4f / 3);
+    }
+
+    // notifies camera to find the player
+    private void OnPlayerSpawned(object playerObject){
+        transform.position = new Vector3( player.position.x, player.position.y, transform.position.z );
+        followSpeed = 3;
     }
 }
