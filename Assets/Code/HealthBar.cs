20db73333a81acaf92cfd4de57a7013b28df864a
Elizabeth Van Elsen <54674146+Lizzie2222000@users.noreply.github.com>
2020-03-20 17:17:41 -0500

    Added Health Bar
    Added a working health bar to game scene that moves with the player's current health.

---
+++ Assets/Code/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    private float scale;
+    public Player player;
+    public GameObject bar;
+    private float position;
+    private float width;
+    private float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectTransform rt = bar.GetComponent<RectTransform>();
+        width = rt.rect.width;
+        height = rt.rect.height;
+        rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, width);
+        rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RectTransform rt = bar.GetComponent<RectTransform>();
+        scale = player.health / player.maxHealth; 
+        rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, width*scale);
+        rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, height);
+    }
+}
